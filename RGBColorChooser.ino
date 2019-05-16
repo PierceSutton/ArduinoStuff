7fce276818485f3b8b8b53c98268b166b198ac86
PierceSutton <33435270+PierceSutton@users.noreply.github.com>
2019-05-15 18:04:01 -0700

    Create RGBColorChooser.ino
    You need the keypad library, an arduino UNO, an RGB LED module and a 4 x 4 keypad for this.
    Plug the first four keypad pins into [13, 12, 7, 6] and plug the other four into [5, 4, 3, 2]. 
    Plug the red pin of the RGB module into 9 , the green pin into 10, the blue pin into 11.

---
+++ RGBColorChooser.ino
@@ -0,0 +1,107 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', NULL},
+  {'4', '5', '6', NULL},
+  {'7', '8', '9', NULL},
+  {NULL, '0', NULL, NULL}
+};
+
+byte rowPins[ROWS] = {13, 12, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char customKey = customKeypad.getKey();
+  int input = 0;
+  int r = 0;
+  int g = 0;
+  int b = 0;
+  int x = 0;
+  int y = 0;
+  while (y == 0) {
+    customKey = customKeypad.getKey();
+    if (customKey && x == 0) {
+      input = customKey - '0';
+      r += input * 100;
+      x++;
+      Serial.println(customKey);
+      delay(100);
+    } else if (customKey && x == 1) {
+      input = customKey - '0';
+      r += input * 10;
+      x++;
+      Serial.println(customKey);
+      delay(100);
+    } else if (customKey && x == 2) {
+      input = customKey - '0';
+      r += input;
+      x = 0;
+      y = 1;
+      Serial.println(customKey);
+      Serial.println(r);
+      delay(100);
+    }
+  }
+  while (y == 1) {
+    customKey = customKeypad.getKey();
+    if (customKey && x == 0) {
+      input = customKey - '0';
+      g += input * 100;
+      x++;
+      Serial.println(customKey);
+      delay(100);
+    } else if (customKey && x == 1) {
+      input = customKey - '0';
+      g += input * 10;
+      x++;
+      Serial.println(customKey);
+      delay(100);
+    } else if (customKey && x == 2) {
+      input = customKey - '0';
+      g += input;
+      x = 0;
+      y = 2;
+      Serial.println(customKey);
+      Serial.println(g);
+      delay(100);
+    }
+  }
+  while (y == 2) {
+    customKey = customKeypad.getKey();
+    if (customKey && x == 0) {
+      input = customKey - '0';
+      b += input * 100;
+      x++;
+      Serial.println(customKey);
+      delay(100);
+    } else if (customKey && x == 1) {
+      input = customKey - '0';
+      b += input * 10;
+      x++;
+      Serial.println(customKey);
+      delay(100);
+    } else if (customKey && x == 2) {
+      input = customKey - '0';
+      b += input;
+      Serial.println(customKey);
+      Serial.println(b);
+      delay(100);
+      analogWrite(9, r);
+      analogWrite(10, g);
+      analogWrite(11, b);
+      y = 0;
+      x = 0;
+      delay(1000);
+    }
+  }
+}
